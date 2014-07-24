187a4e882b3a871ffc83e6f01218ebd1e45d151a
daige <superdaige@gmail.com>
2014-07-24 22:36:28 +0800

    arg_complete

--- nss.py
+++ nss.py
@@ -42,18 +42,43 @@ class NSS(Cmd):
     def do_cls(self,arg):
         self.tools.cls()
 
+    #TODO: 
+    USE_ARG=["Shopex_sess_id_SQLInject",
+             "shopex_4.8.5_api.php_SQLInject"
+             "ms12_020",
+             "B2BBuilder_comment.php_SQLInject",
+             "WordPress_Area53_theme_Arbitrary_File_Upload",
+             "shopex_back_end_SQLInject",
+             "mongodb/"
+             ]
+
     def do_use(self,arg):
         if arg:
             loadModule(arg).cmdloop()
         else:
             self.tools.usage('use')
+    def complete_use(self,text,line,begidx,endidx):
+        if not text:
+            completions = self.USE_ARG[:]
+        else:
+            completions = [i for i in self.USE_ARG  if i.startswith(text)]
+        return completions
+    
+    SHOW_ARG=["all","mongodb"]
 
     def do_show(self,arg):
         if arg :
             self.db.showPlugins(arg)
         else:
             self.tools.usage('show')
-    
+   
+    def complete_show(self,text,line,begidx,endidx):
+        if not text:
+            completions = self.SHOW_ARG[:]
+        else:
+            completions = [i for i in self.SHOW_ARG if i.startswith(text)]
+        return completions
+
     def do_search(self,arg):
         if arg == "":
             self.tools.usage('search')
